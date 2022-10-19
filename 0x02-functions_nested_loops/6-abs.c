5000eeea1894c49aab6c1ef3a40623cacafc0272
D3bbie100 <debraneema@gmail.com>
2022-10-19 05:45:32 -0700

    Print absolute value of numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of a number
+ * @n: number whose absolute value is ro be returned
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
