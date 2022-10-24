3eab9cd04c5cca73aebe84b9e7189dbf9be79811
D3bbie100 <debraneema@gmail.com>
2022-10-24 08:25:00 -0700

    Write 1 to 14 using putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -prints 0 to 14 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int m;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m > 10)
+			{
+				_putchar('1');
+			}
+			_putchar((m % 10) + '0');
+		}
+	_putchar('\n');
+	i++;
+	}
+}
