ccb04dc4455aada9b51c1d54d0ea4d9eba9bf286
D3bbie100 <debraneema@gmail.com>
2022-10-24 16:59:11 -0700

    Fizz Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,20 @@
 int main(void)
 {
 	int i;
-	char s[10] = "Fizz";
-	char st[10] = "Buzz";
-	char str[10] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 		{
-			printf("%s", s);
+			printf("Fizz");
 		}
 		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
-			printf("%s", st);
+			printf("Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s", str);
+			printf("FizzBuzz");
 		}
 		else
 		{
