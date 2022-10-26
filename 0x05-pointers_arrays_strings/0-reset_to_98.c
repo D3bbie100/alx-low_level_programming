475d64eb2cf6aa3c3b507e6aeac19b663573116c
D3bbie100 <debraneema@gmail.com>
2022-10-26 08:43:06 -0700

    Swapping the values of two integer variables

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - update the value a pointer to int point to 98
  * @n: the pointer to int
- * return: Always 0
+ * return: nothing
  */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of integer variables using pointers
+ * @a: pointer to first int variable
+ * @b: pointer to second int variable
+ * return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int  c;
+
+	c = *x;
+	*x = *y;
+	*y = c;
+}
