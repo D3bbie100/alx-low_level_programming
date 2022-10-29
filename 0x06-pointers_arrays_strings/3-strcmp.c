190c8477be03cd9b045d039179b82816c8d66d22
D3bbie100 <debraneema@gmail.com>
2022-10-29 09:20:54 -0700

    Compare lengths of two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,30 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n = 5;
-	int i = 3;
+	int i;
+	int j;
 
-	s1[n] = i;
-	s2[n] = i;
-
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (i < j)
+	{
+		return (-15);
+	}
+	if (i > j)
+	{
+		return (15);
+	}
+	if (i == j)
+	{
+		return (0);
+	}
 	return (0);
 }
