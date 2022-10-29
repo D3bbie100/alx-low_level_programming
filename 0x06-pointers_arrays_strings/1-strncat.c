fa5ef95adcf453b241c392fc2e20a69e4201e0ab
D3bbie100 <debraneema@gmail.com>
2022-10-29 04:47:42 -0700

    Add n bytes of a string onto another

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,9 +9,27 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 3;
+	int i, j, m, l;
 
-	dest[n] = i;
-	src[n] = i;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	l = j;
+	m = n - 1;
+	for (j = 0; j <= m; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+	if (l <= n)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
