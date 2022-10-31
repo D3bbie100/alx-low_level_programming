f8c88a61963077ec3ecabcf97db73d2d6613170e
D3bbie100 <debraneema@gmail.com>
2022-10-30 21:58:15 -0700

    Print exacty without using puts and putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+/**
+ *main - prints a string
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar('a');
+	putchar('n');
+	putchar('d');
+	putchar(' ');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('p');
+	putchar('i');
+	putchar('e');
+	putchar('c');
+	putchar('e');
+	putchar(' ');
+	putchar('o');
+	putchar('f');
+	putchar(' ');
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(' ');
+	putchar('i');
+	putchar('s');
+	putchar(' ');
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+	putchar('l');
+	putchar('"');
+	putchar(' ');
+	putchar('-');
+	putchar(' ');
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(' ');
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(' ');
+	putchar(2 + '0');
+	putchar(0 + '0');
+	putchar(1 + '0');
+	putchar(5 + '0');
+	putchar('-');
+	putchar(1 + '0');
+	putchar(0 + '0');
+	putchar('-');
+	putchar(1 + '0');
+	putchar(9 + '0');
+	putchar('\n');
+	return (1);
+}
