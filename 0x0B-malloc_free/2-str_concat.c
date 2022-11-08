ddf6b273889bac821880caff07471c2324959037
D3bbie100 <debraneema@gmail.com>
2022-11-08 11:18:52 -0800

    Edits

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,14 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, j, m, n;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	i = 0;
 	while (s1[i] != '\0')
 	{
