9db9569d86e842c0f4d3b52c1d68923a9261d648
D3bbie100 <debraneema@gmail.com>
2022-11-08 22:07:27 -0800

    Copy a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,47 +1,20 @@
 #include "main.h"
 
 /**
- * _strncpy - copies n byte string onto another string
- * @dest: string to be copied onto
- * @src: string to be copied onto another
- * @n : size of bytes of src to be copied
- * Return: pointer to dest
+ * _strcpy - copies string
+ * @dest: pointer to string to be copied onto
+ * @src: pointer to string to be copied
+ * Return: Pointer to dest
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i, j, l, m;
+	int i, j;
 
-	m = n - 1;
-	j = 0;
-	while (src[j] != 0)
+	i = 0;
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
-		j++;
+		dest[i] = src[j];
 	}
-	l = j + 1;
-
-	if (l >= n)
-	{
-		i = 0;
-		for (j = 0; j <= m; ++j, ++i)
-		{
-			dest[i] = src[j];
-		}
-	}
-	else
-	{
-		i = 0;
-		for (j = 0; src[j] != '\0'; ++j, ++i)
-		{
-			dest[i] = src[j];
-		}
-		while (j < m)
-		{
-			dest[i] = '\0';
-			++i;
-			++j;
-		}
-		dest[i] = '\0';
-	}
+	dest[i] = '\0';
 	return (dest);
 }
