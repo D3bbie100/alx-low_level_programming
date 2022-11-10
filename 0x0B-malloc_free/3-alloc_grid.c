677256302524eda6ea096a9cc08d96187d9e2674
D3bbie100 <debraneema@gmail.com>
2022-11-10 10:52:12 -0800

    Edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -35,9 +35,12 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (i = 0; j < width; j++)
+	for (i = 0; j < height; j++)
 	{
-		grid[i][j] = 0;
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
 	}
 	return (grid);
 }
