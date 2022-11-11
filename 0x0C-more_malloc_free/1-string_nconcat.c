2816d8b7909e08979642341cdd08c876155a0e59
D3bbie100 <debraneema@gmail.com>
2022-11-11 07:42:55 -0800

    Edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	s = malloc(sizeof(s1) + (sizeof(char) * n));
+	s = malloc(sizeof(s1) - 1 + (sizeof(char) * n));
 	if (s == NULL)
 	{
 		return (NULL);
